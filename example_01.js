246cf45cda71e20038cb98dfc1304b4480308a42
Kunj9.github.io <82100365+Kunj9@users.noreply.github.com>
2023-08-10 14:47:47 -0400

    Add files via upload

---
+++ example_01.js
@@ -0,0 +1,19 @@
+"using strict";
+$ (document).ready( () => {
+    
+    $("#width").focus();
+
+    $("#calc_from").submit( event =>{
+
+        let isValue = true;
+
+        cosnt (width )
+
+    });
+
+
+
+
+
+
+});
